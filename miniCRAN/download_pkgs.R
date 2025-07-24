fd2f7acd965ab02f539d327df88a9881396a937b
mishi42 <140249303+mishi42@users.noreply.github.com>
2025-07-24 11:30:58 +0900

    Create download_pkgs.R

---
+++ miniCRAN/download_pkgs.R
@@ -0,0 +1,61 @@
+library(miniCRAN)
+
+pkgs = c(
+  "checkpoint", "pacman", "rmarkdown", "rticles", "knitr", "DT", "reactable",
+  "knitr", "kableExtra", "Hmisc", "quantreg", "reporttools", "NMOF", "papeR", "ztable", "xtable",
+  "sessioninfo", "quarto", "flextable", "htmlTable", "parameters", "pander",
+  "htmlwidgets", "gt", "gtsummary",
+  "renv", "stargazer", "huxtable",
+  "bookdown", "markdown", "docxtractr", "testthat",
+  "excel.link", "XLConnect", "readxl", "openxlsx", "tinytex",
+  "dbplyr", "DBI", "RODBC", "duckplyr", "arrow", "aws.s3", "bigrquery",
+  "ggh4x", "ggExtra", "lemon", "ggthemes", "hrbrthemes", "patchwork", "plotly", "ggfortify", "ggspatial", "naniar",
+  "colormap", "flexdashboard", "ggridges", "ggdist", "GGally", "ggstatsplot", "ggrepel",
+  "ggraph", "ggupset", "ggcorrplot", "lindia", "ggheatmap", "ggsurvfit", "ggstats", "ggwordcloud", "tidyterra",
+  "vcd", "vcdExtra", "viridis", "ggpubr", "ggsci", "survminer", "ggforce", "cowplot", "ggalt", "ggsignif",
+  "gplots", "modelbased",
+  "shiny", "shinydashboard", "bslib", "shinytest", "shinyFiles",
+  "furrr",
+  "tidylog",
+  "rstan",
+  "rstanarm", "brms", "bayesplot", "bayestestR", "bayesAB", "BART", "MCMCpack", "BLR", "tidybayes", "multilevelmod",
+  "tidyposterior", "dprng", "bartMachine", "broom.mixed", "loo", "rstantools", "shinystan", "projpred", "posterior",
+  "gapminder", "BMA",
+  "rvest", "RSelenium",
+  "corrplot",
+  "Rtsne", "psych", "dirichletprocess", "statmod", "embed",
+  "modelsummary", "skimr", "catdap", "stacks", "bonsai", "glmnet", "vars", "rBaysianOptimization",
+  "tidymodels", "xgboost", "lightgbm", "ranger", "normtest", "lars", "nlme",
+  "partykit", "rpart.plot", "earth", "DataExplorer", "BVAR", "finetune", "sem", "semTools",
+  "semPlot", "lavaan", "lme4", "mclust", "doFuture", "e1071", "glmmTMB", "parameters", "tidyrules",
+  "fixest",
+  "AER", "lmtest", "clubSandwich", "sandwich", "dlm", "KFAS", "bsts", "marginaleffects", "BLPestimatoR", "rms", "plm",
+  "sampleSelection",
+  "CausalImpact", "rdd", "rdrobust", "rddensity", "RDHonest", "DoubleML",
+  "DALEX", "tidytreatment", "Amelia", "ivpack", "MatchIt", "grf", "fwildclusterboot", "survey", "rbounds",
+  "bnlearn", "pcalg", "censReg", "bartCause", "iml", "shapviz", "finalfit", "mice", "simputation", "Matching",
+  "imputeMissings", "Synth", "tidysynth", "gsynth", "panelView", "PanelMatch", "microsynth", "tidyhte",
+  "counterfactuals", "iml","rddapp",
+  "Rdimtools",
+  "VBsparsePCA", "Robyn", "mlogit", "flexmix", "pscl", "arules", "arulesSequences", "arulesViz", "arulesCBA",
+  "conjoint", "bayesm", "invgamma", "rrecsys", "recommenderlab", "recosystem", "NMF",
+  "tidytext",
+  "tm", "stringr", "stringi", "topicmodels", "lda", "LDAvis", "textmineR", "gutenbergr", "methods",
+  "quanteda", "widyr",
+  "tableone",
+  "rnaturalearth",
+  "latex2exp", "distill",
+  "equatiomatic", "ftExtra", "minidown",
+  "polite", "targets",
+  "cronR", "drat",
+  "OpenCL", "spatialsample",
+  "progress", "sen2r", "miniCRAN",
+  "languageserver", "here", "janitor"
+)
+
+pkg_list <- pkgDep(pkgs)
+
+download.packages(pkg_list,destdir='D:\\miniCRAN', type="win.binary")
+
+
+
