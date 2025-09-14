188f8893476b80301b73cd016ab61344aa5f3e2e
mishi42 <140249303+mishi42@users.noreply.github.com>
2025-09-14 23:57:28 +0900

    Update R_ollama.R

--- sample/R_ollama.R
+++ sample/R_ollama.R
@@ -3,4 +3,13 @@
 library(ollamar)
 ollamar::list_models(host = "http://ollama:11434")
 
+library(mall)
+data("reviews")
+
+# apply sentiment analysis to review data
+options(.mall_chat = ellmer::chat_ollama(model = "gemma3:4b",base_url = "http://ollama:11434"))
+
+reviews |>
+  llm_sentiment(review)
+
 
